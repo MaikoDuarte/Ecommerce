9002e027acea4879108d8cbb53ab5222546ba885
Maiko Duarte <mkdu185@gmail.com>
2022-10-27 20:44:40 -0300

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai';
+
+
+const timeNow = new Date().getFullYear();
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p>{timeNow} @Motorpart Baterias - Todos os direitos reservados</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
+  )
+}
+
+export default Footer
