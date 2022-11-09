61077ce64d9bc45965e09cf7dffc3399d7625845
Maiko Duarte <46424572+MaikoDuarte@users.noreply.github.com>
2022-11-09 07:05:27 -0300

    Update Cart.jsx

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -47,14 +47,14 @@ const Cart = () => {
         {cartItems.length < 1 && (
           <div className="empty-cart">
             <AiOutlineShopping size={150} />
-            <h3>Your shopping bag is empty</h3>
+            <h3>Seu carrinho está vazio.</h3>
             <Link href="/">
               <button
                 type="button"
                 onClick={() => setShowCart(false)}
                 className="btn"
               >
-                Continue Shopping
+                Continuar comprando
               </button>
             </Link>
           </div>
