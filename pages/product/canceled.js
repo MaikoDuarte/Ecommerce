125f96b2168b2aff82bbf6e0dd1bc2c0384fde6b
Maiko Duarte <mkdu185@gmail.com>
2022-11-09 07:53:15 -0300

    6 commit

---
+++ pages/product/canceled.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Canceled() {
+    return(
+        <div>
+            <h1>Sua compra foi cancelada</h1>
+        </div>
+    );
+}
+
+export default Canceled;
